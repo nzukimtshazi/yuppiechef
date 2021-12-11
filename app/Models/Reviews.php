5286f09712c020ec87e3ba37ca07ad2925ff256f
nzuki-datax <nzuki.mtshazi@datax.co.za>
2021-12-11 02:19:44 +0200

    create database

---
+++ app/Models/Reviews.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reviews extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['description', 'rating', 'user_name', 'email', 'product_id'];
+}
---
+++ database/migrations/2021_12_11_000917_create_reviews_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('reviews')) {
+            Schema::create('reviews', function (Blueprint $table) {
+                $table->id();
+                $table->string('description');
+                $table->integer('rating');
+                $table->string('user_name');
+                $table->string('email')->unique();
+                $table->unsignedBigInteger('product_id');
+                $table->timestamps();
+            });
+        }
+        Schema::table('reviews', function ($table) {
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
